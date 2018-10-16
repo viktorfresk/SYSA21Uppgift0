a7570ad69dc71ccc5cdbd4fa1012d3d658093084
viktorfresk <44190216+viktorfresk@users.noreply.github.com>
2018-10-16 17:00:00 +0200

    Uppdatering
    skrev till två U:n

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("how much is 2 + 2?");
+		System.out.println("how muuuch is 2 + 2?");
 		System.out.println(2+2);
 	}
 
